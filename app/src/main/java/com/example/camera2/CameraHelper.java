5c49a6c2f9c6161bdf9b917399b5c31b21082700
Andrew Gahov <andrew.gahov@gmail.com>
2018-09-06 21:13:23 +0300

    Part 2

--- app/src/main/java/com/example/camera2/CameraHelper.java
+++ app/src/main/java/com/example/camera2/CameraHelper.java
@@ -1,13 +1,22 @@
 package com.example.camera2;
 
+import android.annotation.SuppressLint;
 import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.util.Arrays;
 
 public class CameraHelper {
 
@@ -15,10 +24,42 @@ public class CameraHelper {
 
     private final CameraManager mCameraManager;
     private final String mCameraId;
+    private final int displayWidth;
+    private final int displayHeight;
+    private CameraDevice mCameraDevice;
+    private final CameraDevice.StateCallback mCameraCallBack;
+    private TextureView mTextureView;
+    private CameraCaptureSession mSession;
 
-    public CameraHelper(@NonNull CameraManager mCameraManager, @NonNull String mCameraId) {
+
+    public CameraHelper(@NonNull CameraManager mCameraManager, @NonNull final String mCameraId,
+                        int displayWidth, int displayHeight) {
         this.mCameraManager = mCameraManager;
         this.mCameraId = mCameraId;
+        this.displayWidth = displayWidth;
+        this.displayHeight = displayHeight;
+
+        mCameraCallBack = new CameraDevice.StateCallback() {
+
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                mCameraDevice = camera;
+                createCameraPreviewSession();
+                Log.d(TAG, "Open camera with id: " + mCameraId);
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+                closeCamera();
+                Log.d(TAG, "disconnect camera with id: " + mCameraId);
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+                Log.e(TAG, "error camera with id: " + mCameraId + ", error = " + error);
+                closeCamera();
+            }
+        };
     }
 
     public void viewFormatSize(int formatSize) {
@@ -41,6 +82,61 @@ public class CameraHelper {
         } catch (CameraAccessException e) {
             e.printStackTrace();
             Log.d(TAG, "CameraAccessException " + e.getMessage());
+        }
+    }
+
+    public boolean isOpen() {
+        return mCameraDevice != null;
+    }
+
+    @SuppressLint("MissingPermission")
+    public void openCamera() {
+        try {
+            mCameraManager.openCamera(mCameraId, mCameraCallBack, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeCamera() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    public void setTextureView(TextureView textureView) {
+        mTextureView = textureView;
+    }
+
+    private void createCameraPreviewSession() {
+        SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(displayWidth, displayHeight);
+        Surface surface = new Surface(surfaceTexture);
+        try {
+            final CaptureRequest.Builder builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            builder.addTarget(surface);
+
+            mCameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    mSession = session;
+                    try {
+                        mSession.setRepeatingRequest(builder.build(), null, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                }
+            }, null);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage());
         }
     }
 }
--- app/src/main/java/com/example/camera2/MainActivity.java
+++ app/src/main/java/com/example/camera2/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.camera2;
 
 import android.content.Context;
 import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Size;
+import android.view.Display;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -17,9 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private CameraManager mCameraManager    = null;
-    CameraHelper[] myCameras = null;
     private int attemptPermissionRequest = 0;
+
+    private CameraManager mCameraManager    = null;
+    private CameraHelper[] myCameras = null;
+    private static final int CAMERA1 = 0;
+    private static final int CAMERA2 = 1;
+    private static Button mBtnCamera1;
+    private static Button mBtnCamera2;
+    private TextureView mTextureView;
+    private int displayWidth;
+    private int displayHeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        displayWidth = displayMetrics.widthPixels;
+        displayHeight = displayMetrics.heightPixels;
+
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        mTextureView = findViewById(R.id.textureView);
+
+        mBtnCamera1 = findViewById(R.id.mBtnCamera1);
+        mBtnCamera2 = findViewById(R.id.mBtnCamera2);
+
+        mBtnCamera1.setOnClickListener(v -> {
+           if (myCameras[CAMERA2].isOpen()) {
+               myCameras[CAMERA2].closeCamera();
+           }
+           if (myCameras[CAMERA1] != null) {
+               if (!myCameras[CAMERA1].isOpen()) {
+                   myCameras[CAMERA1].openCamera();
+               }
+           }
+        });
+
+        mBtnCamera2.setOnClickListener(v -> {
+            if (myCameras[CAMERA1].isOpen()) {
+                myCameras[CAMERA1].closeCamera();
+            }
+            if (myCameras[CAMERA2] != null) {
+                if (!myCameras[CAMERA2].isOpen()) {
+                    myCameras[CAMERA2].openCamera();
+                }
+            }
+        });
+
+        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                closeAllCameras();
+                return true;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
+    }
+
+    private void closeAllCameras() {
+        for (int i = 0; i < myCameras.length; i++) {
+            if (myCameras[i].isOpen()) {
+                myCameras[i].closeCamera();
+            }
+        }
     }
 
     @Override
@@ -47,6 +124,13 @@ public class MainActivity extends AppCompatActivity {
         getCameraList();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        closeAllCameras();
+    }
+
     private void getCameraList() {
         try {
             String[] cameraIdList = mCameraManager.getCameraIdList();
@@ -55,7 +139,8 @@ public class MainActivity extends AppCompatActivity {
             for (String cameraId : cameraIdList) {
                 int id = Integer.parseInt(cameraId);
                 // обработчик по камере
-                myCameras[id] = new CameraHelper(mCameraManager, cameraId);
+                myCameras[id] = new CameraHelper(mCameraManager, cameraId, displayWidth, displayHeight);
+                myCameras[id].setTextureView(mTextureView);
                 // выводим информацию по камере
                 myCameras[id].viewFormatSize(ImageFormat.JPEG);
             }
